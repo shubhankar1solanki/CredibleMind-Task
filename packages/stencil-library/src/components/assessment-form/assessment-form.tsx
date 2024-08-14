9f9b834a8d53b1f4e14e760b138a51061f9debf9
Shubhankar Solanki <shubhankar1solanki@gmail.com>
2024-08-14 17:54:04 +0530

    Update algolia response

--- packages/stencil-library/src/components/assessment-form/assessment-form.tsx
+++ packages/stencil-library/src/components/assessment-form/assessment-form.tsx
@@ -77,11 +77,6 @@ export class AssessmentForm {
     this.index = client.initIndex('algolia-recommendation-data');
   }
 
-  componentDidRender() {
-    this.updateProgress();
-    console.log(this.answers);
-  }
-
   calculateEstimatedTime() {
     const totalQuestions = this.assessmentData.assessmentCollection.items[0].questions.pages.reduce((acc, page) => acc + page.elements.length, 0);
     return (totalQuestions * 30) / 60;
@@ -142,15 +137,19 @@ export class AssessmentForm {
     }
   }
 
-  renderDescription() {
-    const introDescription = this.assessmentData.assessmentCollection.items[0].intro.json.content
-      .slice(1)
+  renderDescription(isResult: boolean = false) {
+    const description = this.assessmentData.assessmentCollection.items[0][isResult ? 'resultsIntro' : 'intro'].json.content
+      .slice(isResult ? 0 : 1)
       .map(content => content.content.map(item => item.value).join(' '))
       .join(' ');
 
+    if (description.length < 100) {
+      return description;
+    }
+
     return this.isDescriptionTruncated ? (
       <span>
-        {introDescription.slice(0, 100)}...{' '}
+        {description.slice(0, 100)}...{' '}
         <span
           class="font-semibold cursor-pointer"
           onClick={() => {
@@ -162,7 +161,7 @@ export class AssessmentForm {
       </span>
     ) : (
       <span>
-        {introDescription}{' '}
+        {description}{' '}
         <span
           class="font-semibold cursor-pointer"
           onClick={() => {
@@ -190,12 +189,12 @@ export class AssessmentForm {
 
   updateProgress() {
     const totalPages = this.assessmentData.assessmentCollection.items[0].questions.pages.length;
-    this.progress = (this.currentPage / totalPages) * 100;
+    this.progress = ((this.currentPage - 1) / totalPages) * 100;
   }
 
   handleNext() {
     const unanswered = this.getUnansweredQuestions();
-    console.log(unanswered);
+    console.log(unanswered, { currentPage: this.currentPage });
     if (unanswered.length > 0) {
       this.scrollToUnanswered(unanswered[0]);
     } else if (this.currentPage < this.assessmentData.assessmentCollection.items[0].questions.pages.length) {
@@ -215,7 +214,6 @@ export class AssessmentForm {
 
   async submitAnswers() {
     try {
-      console.log(this.answers, 'Answers');
       const facetFilters = Object.entries(this.answers)
         .map(([key, value]) => {
           if (Array.isArray(value)) {
@@ -230,7 +228,6 @@ export class AssessmentForm {
       const response = await this.index.search('', { facetFilters: [facetFilters] });
       this.algoliaResults = response.hits;
       this.currentPage++;
-      console.log(this.algoliaResults, 'Results');
       this.completion.emit(this.answers);
     } catch (error) {
       console.error('Algolia search error:', error);
@@ -243,40 +240,62 @@ export class AssessmentForm {
       return <div class="animate-spin"></div>;
     }
 
-    const currentPageData = this.assessmentData.assessmentCollection.items[0].questions.pages[this.currentPage - 1].elements;
-    const introTextTitle = this.assessmentData.assessmentCollection.items[0].intro.json.content[0].content[0].value;
+    if (this.currentPage <= this.assessmentData.assessmentCollection.items[0].questions.pages.length) {
+      const currentPageData = this.assessmentData.assessmentCollection.items[0].questions.pages[this.currentPage - 1].elements;
+      const introTextTitle = this.assessmentData.assessmentCollection.items[0].intro.json.content[0].content[0].value;
+
+      return (
+        <div class="max-w-2xl mx-auto bg-white border border-slate-200 rounded-lg">
+          {/* head */}
+          <div class="bg-[#F8EDEB] p-5 rounded-t-lg">
+            <p class="text-2xl text-[#4B4B4B] font-bold">{introTextTitle}</p>
+            <p class="flex gap-1 mt-3">
+              <img src={ClockSvg} /> <span class="text-[#4B4B4B]">It only takes {this.calculateEstimatedTime()} minutes.</span>
+            </p>
+            <p class="text-base text-[#4B4B4B] mt-3">{this.renderDescription()}</p>
+          </div>
+          {/* Progress */}
+          <div class="absolute h-2 bg-blue-500 rounded-full" style={{ width: `${this.progress}%` }}></div>
+          {/* Questions */}
+          <div class="p-5 shadow-inner">
+            {currentPageData?.map((question, index) => (
+              <div key={index}>{this.renderQuestion(question)}</div>
+            ))}
+          </div>
+
+          {/* Navigation */}
+          <div class="flex justify-between mt-1">
+            <button
+              onClick={() => this.handlePrev()}
+              disabled={this.currentPage === 1}
+              class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l disabled:opacity-50 disabled:cursor-not-allowed"
+            >
+              Previous
+            </button>
+            <button onClick={() => this.handleNext()} class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-r">
+              Next
+            </button>
+          </div>
+        </div>
+      );
+    }
 
     return (
       <div class="max-w-2xl mx-auto bg-white border border-slate-200 rounded-lg">
         {/* head */}
         <div class="bg-[#F8EDEB] p-5 rounded-t-lg">
-          <p class="text-2xl text-[#4B4B4B] font-bold">{introTextTitle}</p>
-          <p class="flex gap-1 mt-3">
-            <img src={ClockSvg} /> <span class="text-[#4B4B4B]">It only takes {this.calculateEstimatedTime()} minutes.</span>
-          </p>
-          <p class="text-base text-[#4B4B4B] mt-3">{this.renderDescription()}</p>
+          <p class="text-base text-[#4B4B4B] mt-3">{this.renderDescription(true)}</p>
         </div>
-        {/* Progress */}
-        <div class="absolute h-2 bg-blue-500 rounded-full" style={{ width: `${this.progress}%` }}></div>
-        {/* Questions */}
-        <div class="p-5 shadow-inner">
-          {currentPageData?.map((question, index) => (
-            <div key={index}>{this.renderQuestion(question)}</div>
+        <div class="mt-6 grid">
+          {this.algoliaResults.map(result => (
+            <div class="mb-4 p-4 border rounded-lg shadow">
+              <img src={result.imageUrl} alt={result.title} class="w-full h-48 object-cover mb-4" />
+              <h3 class="text-xl font-semibold">{result.title}</h3>
+              <p class="text-sm text-gray-600">Author: {result.author}</p>
+              <p class="text-sm text-gray-600">Type: {result.type}</p>
+              <p class="text-sm text-gray-700 mt-2">{result.description}</p>
+            </div>
           ))}
-        </div>
-
-        {/* Navigation */}
-        <div class="flex justify-between mt-1">
-          <button
-            onClick={() => this.handlePrev()}
-            disabled={this.currentPage === 1}
-            class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l disabled:opacity-50 disabled:cursor-not-allowed"
-          >
-            Previous
-          </button>
-          <button onClick={() => this.handleNext()} class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-r">
-            Next
-          </button>
         </div>
       </div>
     );
